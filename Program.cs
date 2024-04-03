9863f742e4a91176e416bee9afae729ade7dd5fc
-ждщшщд Кееее <maxisssjdjxdj@mail.ru>
2024-04-03 22:57:42 +0700

    функуция добавления книги

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
+class Library
+{
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(string title, string author)
+    {
+        Book newBook = new Book { Title = title, Author = author };
+        books.Add(newBook);
+        Console.WriteLine("Книга успешно добавлена.");
+    }
+
+}
+    
