87bd655f8e5f38a26c79afd01e53919bcf12817b
-ждщшщд Кееее <maxisssjdjxdj@mail.ru>
2024-04-03 23:25:40 +0700

    вторая функция

--- Program.cs
+++ Program.cs
@@ -4,8 +4,30 @@ using System.Linq;
 
 class Library
 {
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
+
+    private List<Book> books = new List<Book>();
+
+    public void AddBook(string title, string author)
+    {
+        Book newBook = new Book { Title = title, Author = author };
+        books.Add(newBook);
+        Console.WriteLine("Книга успешно добавлена.");
+    }
+
+
+
+
     public void RemoveBook(string title)
     {
+        
+        
         Book bookToRemove = books.FirstOrDefault(book => book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
         if (bookToRemove != null)
         {
