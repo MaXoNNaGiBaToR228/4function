5690d8637318c2578d6b32991c184ebf3ccb0e8f
-ждщшщд Кееее <maxisssjdjxdj@mail.ru>
2024-04-03 23:03:03 +0700

    вторая функция

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Book
+﻿public void RemoveBook(string title)
 {
-    public string Title { get; set; }
-    public string Author { get; set; }
-}
-
-class Library
-{
-    private List<Book> books = new List<Book>();
-
-    public void AddBook(string title, string author)
+    Book bookToRemove = books.FirstOrDefault(book => book.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+    if (bookToRemove != null)
     {
-        Book newBook = new Book { Title = title, Author = author };
-        books.Add(newBook);
-        Console.WriteLine("Книга успешно добавлена.");
+        books.Remove(bookToRemove);
+        Console.WriteLine("Книга успешно удалена.");
     }
-
+    else
+    {
+        Console.WriteLine("Книга не найдена.");
+    }
 }
-    
